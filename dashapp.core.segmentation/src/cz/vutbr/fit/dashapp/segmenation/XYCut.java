ea9e675a3a0227f4e355b157c61ca89c8f7dea49
Jiri Hynek <xhynek@gmail.com>
2017-11-22 05:41:55 -0500

    Segmentation : XY cut template added

---
+++ dashapp.core.segmentation/src/cz/vutbr/fit/dashapp/segmenation/XYCut.java
@@ -0,0 +1,20 @@
+package cz.vutbr.fit.dashapp.segmenation;
+
+import java.awt.image.BufferedImage;
+
+import cz.vutbr.fit.dashapp.model.Dashboard;
+
+public class XYCut implements ISegmentationAlgorithm {
+
+	@Override
+	public Dashboard processImage(BufferedImage image) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "XY-cut";
+	}
+
+}
